2edd9b8a43bbe032551918d00ebaf735f832b956
protove <sbdl12342@gmail.com>
2026-07-15 22:04:52 +0900

    feat: 국가·도시 기준정보 조회 API 구현 (#16)

---
+++ backend/src/main/kotlin/com/ktcloud/travelplanner/location/repository/CountryRepository.kt
@@ -0,0 +1,19 @@
+package com.ktcloud.travelplanner.location.repository
+
+import com.ktcloud.travelplanner.location.model.Country
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+
+interface CountryRepository : JpaRepository<Country, Short> {
+	@Query(
+		"""
+		SELECT country
+		FROM Country country
+		WHERE country.isActive = true
+		ORDER BY country.displayOrder, country.nameKo, country.id
+		""",
+	)
+	fun findActive(): List<Country>
+
+	fun existsByIdAndIsActiveTrue(countryId: Short): Boolean
+}
