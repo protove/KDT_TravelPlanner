62b23c97118d3d7afe9495f93a88f1f3bd873513
aragonhyeok <aragonhyeok@khu.ac.kr>
2026-07-20 16:31:00 +0900

    test: 타임라인 마이그레이션 테스트에 target 버전 명시 (V7 추가로 인한 실패 수정)

--- backend/src/test/kotlin/com/ktcloud/travelplanner/timeline/repository/TimelineMigrationIntegrationTest.kt
+++ backend/src/test/kotlin/com/ktcloud/travelplanner/timeline/repository/TimelineMigrationIntegrationTest.kt
@@ -32,7 +32,7 @@ class TimelineMigrationIntegrationTest : ContainerIntegrationTestSupport() {
 				.dataSource(dataSource)
 				.schemas(schema)
 				.defaultSchema(schema)
-				.target(MigrationVersion.fromVersion("5"))
+				.target(MigrationVersion.fromVersion("6"))
 				.load()
 				.migrate()
 
